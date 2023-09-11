a85bb58230d962d42e4912f134f1bb1902100354
Dangtae <youngtae0818@naver.com>
2023-09-11 16:24:18 +0900

    Start project

---
+++ chatpter6_03_summary/backend.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+
+openai.api_key = "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"
+
+def summarize(text):
+    system_instruction = "asistant는 user의 입력을 bullet point로 3줄 요약해준다."
+
+    messages = [{"role": "system", "content": system_instruction},
+               {"role": "user", "content": text}
+               ]
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = messages)
+    result  = response['choices'][0]['message']['content']
+    return result
+
+
+app = FastAPI()
+
+class InputText(BaseModel):
+    text: str
+
+@app.post("/summarize")
+def post_summarize(input_text: InputText):
+    summary = summarize(input_text.text)
+    return {"summary": summary}
